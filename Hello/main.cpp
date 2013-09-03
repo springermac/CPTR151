dbec5f588c02a02591fee84c74331d02c8ea6dd4
Jonathan Springer <springermac@gmail.com>
2013-09-03 19:28:53 -0400

    Initial

--- Hello/main.cpp
+++ Hello/main.cpp
@@ -1,18 +1,29 @@
-//
-//  main.cpp
-//  Hello
-//
-//  Created by Jonathan Springer on 9/3/13.
-//  Copyright (c) 2013 Jonathan Springer. All rights reserved.
-//
+#include "iostream"
+using namespace std;
 
-#include <iostream>
-
-int main(int argc, const char * argv[])
+int main()
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int age;//age of individual
+    string fname;//first name of individual
+    string lastName;//last name of individual
+    
+    //get the first name of individual
+    cout<<"\n\nEnter your first name: ";
+    cin>>fname;
+    
+    //get the age of individual
+    cout<<"Enter your age ";
+    cin>>age;
+    
+    //get the last name of the individual
+    cout<<"Enter your last name: ";
+    cin>>lastName;
+    
+    
+    //display results
+    cout<<"\\nHello " <<fname<<""<<lastName;
+    cout<<"your age is "<<age<<" Really????";
+    cout<<"\n\n\n";
     return 0;
-}
-
+    
+}//end main function
