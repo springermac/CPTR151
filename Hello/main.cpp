8ef48dcd03af9f18e5e33626ff585e25e2c6153f
Jonathan Springer <springermac@gmail.com>
2013-09-03 19:01:47 -0400

    Initial Commit

---
+++ Hello/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hello
+//
+//  Created by Jonathan Springer on 9/3/13.
+//  Copyright (c) 2013 Jonathan Springer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
