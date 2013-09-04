e36636fb9ec6d8a1775c91fc5cc79ffb82b2bf74
Jonathan Springer <springermac@gmail.com>
2013-09-03 20:11:57 -0400

    Add peasPerPod program

---
+++ dis/dis/peasPerPod.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  dis
+//
+//  Created by Jonathan Springer on 9/3/13.
+//  Copyright (c) 2013 Jonathan Springer. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numberOfPods;
+    int peasPerPod;
+    int totalPeas;
+    
+    cout<<"Press return after entering a number.\n";
+    cout<<"Enter the number of pods:\n";
+    
+    cin>>numberOfPods;
+    
+    cout<<"Enter the number of peas per pod:\n";
+    cin>>peasPerPod;
+    
+    totalPeas=numberOfPods*peasPerPod;
+    
+    cout<<"If you have "<<numberOfPods<<" pea pods\n";
+    cout<<"and "<<peasPerPod<<" peas in each pod, then\n";
+    cout<<"you have "<<totalPeas<<" peas in all the pods.";
+    
+    return 0;
+}
+
