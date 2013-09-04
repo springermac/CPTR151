f46842fef43b9c4a569b365cb7de03b06be3821e
Jonathan Springer <springermac@gmail.com>
2013-09-04 09:52:40 -0400

    Add program coinCounter. coinCounter takes the number of quarters, dimes, and nickels and outputs the total monetary value.

---
+++ coinCounter/coinCounter/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  coinCounter
+//
+//  Collect number of quarters, nickles, and dimes input by keyboard then calculate and display total monetary value.
+//
+//  Created by Jonathan Springer on 9/4/13.
+//  Copyright (c) 2013 Jonathan Springer. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int quarters;
+    int dimes;
+    int nickles;
+    int total;
+    
+    cout<<"\nEnter number of quarters: ";
+    cin>>quarters;
+    
+    cout<<"Enter number of dimes: ";
+    cin>>dimes;
+    
+    cout<<"Enter number of nickles: ";
+    cin>>nickles;
+    
+    total=(quarters*25)+(dimes*10)+(nickles*5);
+    
+    cout<<"Total value: "<<total;
+    
+    return 0;
+}
+
