3d486b862a9853294ff98c400aeade5ca2bb6d25
Jonathan Springer <springermac@gmail.com>
2013-09-05 19:08:25 -0400

    Add HelloWorld in C++

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Jonathan Springer on 9/5/13.
+//  Copyright (c) 2013 Jonathan Springer. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for (i=0; i<10; i=i+1)
+    {
+        cout<<("\n Hello World");
+    }
+}
