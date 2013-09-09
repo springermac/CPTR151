1cc6188caa0275f11b4b6967f6cfa952ed25f5cb
Jonathan Springer <springermac@gmail.com>
2013-09-09 00:09:34 -0400

    Update comments

--- peasPerPod/peasPerPod/peasPerPod.cpp
+++ peasPerPod/peasPerPod/peasPerPod.cpp
@@ -5,29 +5,32 @@
 //  Created by Jonathan Springer on 9/3/13.
 //  Copyright (c) 2013 Jonathan Springer. All rights reserved.
 //
+//  Description: This program calculates the number of peas given the number of pods and the number of peas per pod.
+//
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int numberOfPods;
-    int peasPerPod;
-    int totalPeas;
+    int numberOfPods;//Variable to hold the number of pods
+    int peasPerPod;//Variable to hold the number of peas per pod
+    int totalPeas;//Variable to hold the total number of peas
     
-    cout<<"Press return after entering a number.\n";
-    cout<<"Enter the number of pods:\n";
+    cout<<endl<<"Press return after entering a number."<<endl;//Give instructions on how to enter the number of pods and peas per pod
+    cout<<"Enter the number of pods:"<<endl;//Prompts for the number of pods
     
-    cin>>numberOfPods;
+    cin>>numberOfPods;//Stores the input form the console to the variable numberOfPods
     
-    cout<<"Enter the number of peas per pod:\n";
-    cin>>peasPerPod;
+    cout<<"Enter the number of peas per pod:"<<endl;//Prompts for the number of peas per pod
+    cin>>peasPerPod;//Stores the input form the console to the variable peasPerPod
     
+    /*Calculates the total number of peas by multiplying the number of pods by the number of peas per pod*/
     totalPeas=numberOfPods*peasPerPod;
     
-    cout<<"If you have "<<numberOfPods<<" pea pods\n";
-    cout<<"and "<<peasPerPod<<" peas in each pod, then\n";
-    cout<<"you have "<<totalPeas<<" peas in all the pods.";
+    cout<<"If you have "<<numberOfPods<<" pea pods"<<endl;//Displays the number of pods
+    cout<<"and "<<peasPerPod<<" peas in each pod, then"<<endl;//Displays the number of peas per pod
+    cout<<"you have "<<totalPeas<<" peas in all the pods.";//Displays the total number of peas
     
     return 0;
 }
