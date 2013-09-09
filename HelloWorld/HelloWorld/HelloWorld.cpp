539b94690df00c5855804c7ebf8b1fb54c0ee543
Jonathan Springer <springermac@gmail.com>
2013-09-09 00:01:33 -0400

    Update comments

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -5,15 +5,17 @@
 //  Created by Jonathan Springer on 9/5/13.
 //  Copyright (c) 2013 Jonathan Springer. All rights reserved.
 //
+//  Description: This program prints out "Hello World" 20 times to the console.
+//
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int i;
-    for (i=0; i<10; i=i+1)
+    int i;//Variable to hold the count of how many times the for loop has been run
+    for (i=0; i<20; i=i+1)//For loop that loops 20 times
     {
-        cout<<("Hello World")<<endl;
+        cout<<endl<<("Hello World")<<endl;//Prints "Hello World" to the console
     }
 }
