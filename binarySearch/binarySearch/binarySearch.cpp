d18ffeeb76b54caa342d01a5a2a36a5752a4ce49
Jonathan Springer <springermac@gmail.com>
2013-10-24 09:32:07 -0400

    Add example programs

---
+++ binarySearch/binarySearch/binarySearch.cpp
@@ -0,0 +1,18 @@
+//
+//  binarySearch.cpp
+//  binarySearch
+//
+//  Created by Jonathan Springer on 10/24/13.
+//  Copyright (c) 2013 Jonathan Springer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
