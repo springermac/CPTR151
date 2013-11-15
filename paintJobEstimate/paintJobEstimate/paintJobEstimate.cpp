c67eb3dbaeae97898e474c931073466f8b5cd126
Jonathan Springer <springermac@gmail.com>
2013-11-14 20:07:38 -0500

    Add paintJobEstimate

---
+++ paintJobEstimate/paintJobEstimate/paintJobEstimate.cpp
@@ -0,0 +1,25 @@
+//
+//  paintJobEstimate.cpp
+//  paintJobEstimate
+//
+//  Created by Jonathan Springer on 11/14/13.
+//  Copyright (c) 2013 Jonathan Springer. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const float COST_OF_LABOR_PER_HOUR=18.00;
+	const int WALL_AREA=115;
+	const int GALLONS_USED_PER_WALL=1;
+	const int HOURS_OF_LABOR_REQUIRED_PER_WALL=8;
+	float gallons;
+	float hours;
+	float costOfPaint;
+	float totalCost;
+	
+    return 0;
+}
+
